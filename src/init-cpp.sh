4ab9653606e2b4871e47e31842fd59eac6af49b5
Kacper Kafara <kacperkafara@gmail.com>
2021-10-06 21:58:52 +0200

    [src][cpp] Add helper functions && improve usage message
    * log_and_exit
    * assert_dir_exists
    * assert_equals

--- src/init-cpp.sh
+++ src/init-cpp.sh
@@ -6,6 +6,7 @@ templ_dir=${TEMPLATES_PATH}/cpp-templ
 ret_success=0
 ret_fnf=1
 ret_bad_arg_count=2
+ret_assert_err=3
 
 # OPTIONS
 opt_str="hvd:"
@@ -13,7 +14,7 @@ opt_str="hvd:"
 arg_count=1
 
 usage() { 
-  echo "Usage: $0 [-h (prints this help message)] [-v (verbose mode)] [-d <path> (directory to init)] <project name>"; 
+  echo "Usage: $0 [-h (prints this help message)] [-v (verbose mode)] [-d <path> (parent directory of new project)] <project name>";
   exit ${ret_success}; 
 }
 
@@ -24,6 +25,23 @@ info() {
 
 log() {
 	[ "$1" -eq 0 ] && echo "$2"
+}
+
+log_and_exit() {
+	echo "$1";
+	exit "$2";
+}
+
+assert_dir_exists() {
+	if [[ ! -d "$1" ]]; then
+		log_and_exit "Directory $1 not found. Make sure that the directory exists." "${ret_fnf}"
+	fi
+}
+
+assert_equals() {
+	if [[ "$1" -ne "$2" ]]; then
+		log_and_exit "Assertion failed. Received: $1. Expected: $2" "${ret_assert_err}"
+	fi
 }
 
 while getopts ${opt_str} opts;
@@ -47,18 +65,8 @@ shift "$(( $OPTIND - 1 ))"
 verbose=${verbose:=1}
 project_parent_dir=${project_parent_dir:=$(pwd)}
 
-if [[ ! -f ${TEMP_MAKEFILE_PATH} ]]; then
-  echo "File ${TEMP_MAKEFILE_PATH} not found. Ensure that correct path is provided in script source."
-  exit ${ret_fnf}
-elif [[ ! -f ${TEMP_CFILE_PATH} ]]; then
-  echo "File ${TEMP_CFILE_PATH} not found. Ensure that correct path is provided in script source."
-  exit ${ret_fnf}
-fi
-
-if [[ ! -d ${project_parent_dir} ]]; then
-  echo "Directory ${project_parent_dir} not found. Ensure that the directory exists."
-  exit ${ret_fnf}
-fi
+assert_dir_exists "${templ_dir}"
+assert_dir_exists "${project_parent_dir}"
 
 if [[ $# -ne ${arg_count} ]]; then
   echo "Invalid arg count. Expected: ${arg_count}."
