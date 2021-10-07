3e84f53085707af329404b203db71b7273e22550
Kacper Kafara <kacperkafara@gmail.com>
2021-10-08 01:46:42 +0200

    [src] Handle creation of `c` projects by `init-proj` script

--- src/init-proj.sh
+++ src/init-proj.sh
@@ -6,27 +6,36 @@ if [[ -z ${SCRIPTS_DIR} ]]; then
 	exit -1
 fi
 
-templ_dir=${SCRIPTS_DIR}/templates/cpp-templ
-arg_count=1
+arg_count=2 # project-type, project-name
 
 source ${SCRIPTS_DIR}/src/return-codes.sh
 source ${SCRIPTS_DIR}/src/utils.sh
 
+usage="Usage: $0 [OPTIONS] PROJECT_NAME PROJECT_TYPE\n
+PROJECT_TYPE:\n
+\tcpp\n
+\tc\n
+OPTIONS:\n
+\t-h (print help message and exit)\n
+\t-v (verbose mode)\n
+\t-d DIR_PATH (directory to init project in; defaults to result of pwd)"
+
+
 usage() { 
-  echo "Usage: $0 [-h (prints this help message)] [-v (verbose mode)] [-d <path> (parent directory of new project)] <project name>";
+  echo -e ${usage}
 }
 
 info() {
   echo "Initializing $1 directory";
 }
 
-opt_str="hvd:"
-while getopts ${opt_str} opts;
+opt_str="t:hvd:"
+while getopts ${opt_str} opt;
 do
-  case $opts in
+  case $opt in
     "h") 
       usage
-			echo ${ret_success}
+			exit ${ret_success}
       ;;
     "v")
       verbose=0
@@ -42,12 +51,27 @@ shift "$(( $OPTIND - 1 ))"
 verbose=${verbose:=1}
 project_parent_dir=${project_parent_dir:=$(pwd)}
 
-assert_dir_exists "${templ_dir}"
 assert_dir_exists "${project_parent_dir}"
 assert_equals $# ${arg_count} "Invalid arg count. Expected: ${arg_count}"
 
 project_name=$1
 project_dir=${project_parent_dir}/${project_name}
+
+source ${SCRIPTS_DIR}/src/templ-paths.sh
+
+project_type=$2
+case ${project_type} in
+	"cpp")
+		templ_dir=${__templ_cpp__}
+		;;
+	"c")
+		templ_dir=${__templ_c__}
+		;;
+	*)
+		log_and_exit "Invalid project type \"${project_type}\". See usage for valid ones." "${ret_invalid_arg}"
+esac
+
+assert_dir_exists "${templ_dir}"
 
 log ${verbose} "Initializing project of name ${project_name} in ${project_parent_dir} directory."
  
