3bdf14b8e2982b38985e827988b600d504a40ac3
Kacper Kafara <kacperkafara@gmail.com>
2021-10-08 01:48:30 +0200

    [src] Add `assert_variable_defined` function

--- src/utils.sh
+++ src/utils.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+__ret_assert_err__=9
+
 log() {
-	[ "$1" -eq 0 ] && echo "$2"
+	[ "$1" -eq 0 ] && echo -e "$2"
 }
 
 log_and_exit() {
-	echo "$1";
+	echo -e "$1";
 	exit "$2";
 }
 
 log_and_exit_opt() {
-	echo "$1";
+	echo -e "$1";
 	if [[ ! -z "$3" ]]; then
-		echo "$3";
+		echo -e "$3";
 	fi
 	exit "$2";
 }
@@ -22,7 +24,7 @@ log_and_exit_opt() {
 #		2 -- message (optional)
 assert_dir_exists() {
 	if [[ ! -d "$1" ]]; then
-		log_and_exit_opt "Assertion failed: assert_dir_exists: Directory $1 not found. Make sure that the directory exists." "${ret_fnf}" "$2"
+		log_and_exit_opt "Assertion failed: assert_dir_exists: Directory $1 not found. Make sure that the directory exists." "${__ret_assert_err__}" "$2"
 	fi
 }
 
@@ -32,7 +34,7 @@ assert_dir_exists() {
 #		3 -- message (optional)
 assert_equals() {
 	if [[ "$1" -ne "$2" ]]; then
-		log_and_exit_opt "Assertion failed: assert_equals. Received: $1. Expected: $2" "${ret_assert_err}" "$3"
+		log_and_exit_opt "Assertion failed: assert_equals. Received: $1. Expected: $2" "${__ret_assert_err__}" "$3"
 	fi
 }
 
@@ -42,6 +44,17 @@ assert_equals() {
 # 	3 -- message (optional)
 assert_not_equals() {
 	if [[ "$1" -eq "$2" ]]; then
-		log_and_exit_opt "Assertion failed: assert_not_equals. Received $1. Expected: not $2" "${ret_assert_err}" "$3"
+		log_and_exit_opt "Assertion failed: assert_not_equals. Received $1. Expected: not $2" "${__ret_assert_err__}" "$3"
 	fi
 }
+
+# params:
+#		1 -- variable value
+#		2 -- message (optional)
+assert_variable_defined() {
+	if [[ ! -z "$1" ]]; then
+		log_and_exit_opt "Assertion failed: variable does not exist" "${__ret_assert_err__}" "$2"
+	fi
+}
+
+unset __ret_assert_err__
